695c5a69a6961e091c29105220b24b6e2dd47827
marth141 <ncasados@live.com>
2023-12-16 18:31:57 -0700

    Adding observer

--- mix.exs
+++ mix.exs
@@ -19,7 +19,7 @@ defmodule BetBuddies.MixProject do
   def application do
     [
       mod: {BetBuddies.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools, :observer, :wx]
     ]
   end
 
