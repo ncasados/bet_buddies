c7efd90d7531881483b3317e2164334123de303f
marth141 <ncasados@live.com>
2024-01-08 15:02:18 -0700

    Adding a bunch of typespecs to poker module

--- lib/poker/poker.ex
+++ lib/poker/poker.ex
@@ -1,35 +1,43 @@
 defmodule Poker do
+  alias Poker.GameState
   alias Poker.Card
   alias Ecto.UUID
 
+  @spec bet(binary(), binary(), integer()) :: %GameState{}
   def bet(game_id, player_id, amount) do
     [{pid, nil}] = Registry.lookup(Poker.GameRegistry, game_id)
     Poker.GameSession.bet(pid, player_id, amount)
   end
 
+  @spec new_game_id() :: binary()
   def new_game_id() do
     UUID.generate()
   end
 
+  @spec start_game(binary()) :: %GameState{}
   def start_game(game_id) do
     [{pid, nil}] = Registry.lookup(Poker.GameRegistry, game_id)
     Poker.GameSession.start(pid)
   end
 
+  @spec get_game_state(binary()) :: %GameState{}
   def get_game_state(game_id) do
     [{pid, nil}] = Registry.lookup(Poker.GameRegistry, game_id)
     Poker.GameSession.read(pid)
   end
 
+  @spec join_game(binary(), binary(), binary()) :: %GameState{}
   def join_game(game_id, player_id, player_name) do
     [{pid, nil}] = Registry.lookup(Poker.GameRegistry, game_id)
     Poker.GameSession.join(pid, player_id, player_name)
   end
 
+  @spec create_game(binary(), binary(), binary()) :: :ignore | {:error, any()} | {:ok, pid()} | {:ok, pid(), any()}
   def create_game(game_id, player_id, player_name) do
     Poker.GameSupervisor.create_game(game_id, player_id, player_name)
   end
 
+  @spec new_shuffled_deck() :: [%Card{}]
   def new_shuffled_deck() do
     suits = ["spade", "heart", "club", "diamond"]
     values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
