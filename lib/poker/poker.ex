b99f228e34faf7e774a48b0601de6d85c5c16490
marth141 <ncasados@live.com>
2024-01-29 20:17:09 -0700

    Adding poker.call function to the poker module

--- lib/poker/poker.ex
+++ lib/poker/poker.ex
@@ -3,6 +3,11 @@ defmodule Poker do
   alias Poker.Card
   alias Ecto.UUID
 
+  def call(game_id, player_id, amount) do
+    [{pid, nil}] = Registry.lookup(Poker.GameRegistry, game_id)
+    Poker.GameSession.call(pid, player_id, amount)
+  end
+
   def fold(game_id, player_id) do
     [{pid, nil}] = Registry.lookup(Poker.GameRegistry, game_id)
     Poker.GameSession.fold(pid, player_id)
