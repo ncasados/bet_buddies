ef8dee4e28663697d19a9c7039e7f57deb68561e
marth141 <ncasados@live.com>
2024-02-06 15:38:44 -0700

    Adding more tests

--- test/poker/poker_test.exs
+++ test/poker/poker_test.exs
@@ -1,8 +1,32 @@
-alias Erl2exVendored.Pipeline.ExFunc
 ExUnit.start()
 
 defmodule Poker.PokerTest do
   use ExUnit.Case, async: true
+
+  test "player cannot bet while game is not started" do
+    assert {:ok, pid} = Poker.create_game("test_game_id", "test_player_id", "test_player")
+    assert :game_not_active = Poker.bet("test_game_id", "test_player_id", 21000)
+    assert :ok = DynamicSupervisor.terminate_child(Poker.GameSupervisor, pid)
+  end
+
+  test "player starts with 20,000" do
+    assert {:ok, pid} = Poker.create_game("test_game_id", "test_player_id", "test_player")
+    assert %Poker.GameState{players: [%Poker.Player{wallet: wallet}]} = Poker.GameSession.read(pid)
+    assert 20000 = wallet
+    assert :ok = DynamicSupervisor.terminate_child(Poker.GameSupervisor, pid)
+  end
+
+  test "player is added to game when game is created" do
+    assert {:ok, pid} = Poker.create_game("test_game_id", "test_player_id", "test_player")
+    assert %Poker.GameState{players: players} = Poker.GameSession.read(pid)
+    assert 1 = length(players)
+    assert :ok = DynamicSupervisor.terminate_child(Poker.GameSupervisor, pid)
+  end
+
+  test "game can be created" do
+    assert {:ok, pid} = Poker.create_game("test_game_id", "test_player_id", "test_player")
+    assert :ok = DynamicSupervisor.terminate_child(Poker.GameSupervisor, pid)
+  end
 
   test "Two shuffled decks are not the same" do
     assert Poker.new_shuffled_deck() != Poker.new_shuffled_deck()
