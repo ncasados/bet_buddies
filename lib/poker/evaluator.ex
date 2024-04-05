bdc4acf251663769f95ba1deea7b9d68277c7a75
Nathan Casados <ncasados@live.com>
2024-04-04 19:29:08 -0600

    Finishing up hands in evaluator

--- lib/poker/evaluator.ex
+++ lib/poker/evaluator.ex
@@ -56,9 +56,7 @@ defmodule Poker.Evaluator do
         with {true, _cards} <-
                Enum.sort_by(cards, fn %Card{} = card -> card.low_numerical_value end)
                |> Enum.chunk_every(5, 1, :discard)
-               |> IO.inspect()
                |> Enum.map(&five_in_sequence?(&1))
-               |> IO.inspect()
                |> Enum.reject(fn {key, _cards} -> key == false end)
                |> List.last() do
           :straight_flush_exists
@@ -68,71 +66,115 @@ defmodule Poker.Evaluator do
     end
   end
 
-  def five_in_sequence?(list), do: check_sequence(list, nil, [])
-  def check_sequence([], card_ah, acc), do: {true, [card_ah | acc]}
-  def check_sequence([%Card{} = card_h | t], nil, acc), do: check_sequence(t, card_h, acc)
+  def four_of_a_kind?(list_of_cards) do
+    card_values_group =
+      Enum.group_by(list_of_cards, fn %Card{} = card -> card.literal_value end)
+      |> Enum.filter(fn {_literal_values, cards} -> Enum.count(cards) == 4 end)
 
-  def check_sequence([%Card{} = card_h | t], %Card{} = card_ah, acc)
-      when card_ah.low_numerical_value == card_h.low_numerical_value - 1,
-      do: check_sequence(t, card_h, [card_ah | acc])
+    case card_values_group do
+      [] ->
+        :no_four_of_a_kind
 
-  def check_sequence(_, _, acc), do: {false, acc}
-
-  def a_straight_flush() do
-    Poker.new_shuffled_deck()
-    |> Enum.reduce([], fn
-      %Card{suit: "spade", literal_value: "10"} = card, acc ->
-        [card] ++ acc
-
-      %Card{suit: "spade", literal_value: "9"} = card, acc ->
-        [card] ++ acc
-
-      %Card{suit: "spade", literal_value: "8"} = card, acc ->
-        [card] ++ acc
-
-      %Card{suit: "spade", literal_value: "7"} = card, acc ->
-        [card] ++ acc
-
-      %Card{suit: "spade", literal_value: "6"} = card, acc ->
-        [card] ++ acc
-
-      %Card{suit: "spade", literal_value: "5"} = card, acc ->
-        [card] ++ acc
-
-      %Card{suit: "spade", literal_value: "4"} = card, acc ->
-        [card] ++ acc
-
-      _card, acc ->
-        acc
-    end)
+      [{_suit, _cards}] ->
+        :four_of_a_kind_exists
+    end
   end
 
-  def a_real_royal_flush() do
-    Poker.new_shuffled_deck()
-    |> Enum.reduce([], fn
-      %Card{suit: "spade", literal_value: "A"} = card, acc ->
-        [card] ++ acc
+  def full_house?(list_of_cards) do
+    card_values_group =
+      Enum.group_by(list_of_cards, fn %Card{} = card -> card.literal_value end)
+      |> Enum.filter(fn
+        {_literal_values, cards} when length(cards) == 3 -> true
+        {_literal_values, cards} when length(cards) == 2 -> true
+        {_literal_values, _cards} -> false
+      end)
+      |> Enum.map(fn {_key, cards} -> Enum.count(cards) end)
 
-      %Card{suit: "spade", literal_value: "K"} = card, acc ->
-        [card] ++ acc
-
-      %Card{suit: "spade", literal_value: "Q"} = card, acc ->
-        [card] ++ acc
-
-      %Card{suit: "spade", literal_value: "J"} = card, acc ->
-        [card] ++ acc
-
-      %Card{suit: "spade", literal_value: "10"} = card, acc ->
-        [card] ++ acc
-
-      %Card{suit: "diamond", literal_value: "A"} = card, acc ->
-        [card] ++ acc
-
-      %Card{suit: "club", literal_value: "A"} = card, acc ->
-        [card] ++ acc
-
-      _card, acc ->
-        acc
-    end)
+    if Enum.member?(card_values_group, 2) and Enum.member?(card_values_group, 3) do
+      :a_full_house_exists
+    else
+      :no_full_house
+    end
   end
+
+  def flush?(list_of_cards) do
+    card_suit_group =
+      Enum.group_by(list_of_cards, fn %Card{} = card -> card.suit end)
+      |> Enum.filter(fn {_suit, cards} -> Enum.count(cards) >= 5 end)
+
+    case card_suit_group do
+      [] -> :no_flush
+      _ -> :a_flush_exists
+    end
+  end
+
+  def straight?(list_of_cards) do
+    with {true, _cards} <-
+           Enum.sort_by(list_of_cards, fn %Card{} = card -> card.low_numerical_value end)
+           |> Enum.chunk_every(5, 1, :discard)
+           |> Enum.map(&five_in_sequence?(&1))
+           |> Enum.reject(fn {key, _cards} -> key == false end)
+           |> List.last() do
+      :straight_exists
+    else
+      nil -> :no_straight_exists
+    end
+  end
+
+  def three_of_a_kind?(list_of_cards) do
+    card_values_group =
+      Enum.group_by(list_of_cards, fn %Card{} = card -> card.literal_value end)
+      |> Enum.filter(fn {_literal_values, cards} -> Enum.count(cards) == 3 end)
+
+    case card_values_group do
+      [] ->
+        :no_three_of_a_kind
+
+      [{_suit, _cards}] ->
+        :three_of_a_kind_exists
+    end
+  end
+
+  def two_pair?(list_of_cards) do
+    card_values_group =
+      Enum.group_by(list_of_cards, fn %Card{} = card -> card.literal_value end)
+      |> Enum.filter(fn {_literal_values, cards} -> Enum.count(cards) == 2 end)
+
+    case length(card_values_group) do
+      2 ->
+        :two_pair_exists
+
+      _ ->
+        :no_two_pair
+    end
+  end
+
+  def one_pair?(list_of_cards) do
+    card_values_group =
+      Enum.group_by(list_of_cards, fn %Card{} = card -> card.literal_value end)
+      |> Enum.filter(fn {_literal_values, cards} -> Enum.count(cards) == 2 end)
+
+    case length(card_values_group) do
+      1 ->
+        :pair_exists
+
+      _ ->
+        :no_pair
+    end
+  end
+
+  def high_card?(list_of_cards) do
+    Enum.sort_by(list_of_cards, fn %Card{} = card -> card.high_numerical_value end)
+    |> List.last()
+  end
+
+  defp five_in_sequence?(list), do: check_sequence(list, nil, [])
+  defp check_sequence([], card_ah, acc), do: {true, [card_ah | acc]}
+  defp check_sequence([%Card{} = card_h | t], nil, acc), do: check_sequence(t, card_h, acc)
+
+  defp check_sequence([%Card{} = card_h | t], %Card{} = card_ah, acc)
+       when card_ah.low_numerical_value == card_h.low_numerical_value - 1,
+       do: check_sequence(t, card_h, [card_ah | acc])
+
+  defp check_sequence(_, _, acc), do: {false, acc}
 end
