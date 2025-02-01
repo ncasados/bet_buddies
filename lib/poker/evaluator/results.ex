466b7832c769f891451a5837fdbf496b980aa2b3
Nathan Casados <ncasados@live.com>
2025-01-31 20:39:09 -0700

    Adding type to results struct

--- lib/poker/evaluator/results.ex
+++ lib/poker/evaluator/results.ex
@@ -1,4 +1,11 @@
 defmodule Poker.Evaluator.Results do
+  @type t() :: %__MODULE__{
+          type: atom(),
+          exists?: boolean(),
+          cards: list(Poker.Card.t()),
+          index: integer()
+        }
+
   defstruct [
     :type,
     :exists?,
