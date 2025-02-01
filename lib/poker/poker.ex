436952a0a883386c4949bfb8c6cbb8fd7b6d280c
Nathan Casados <ncasados@live.com>
2025-01-31 19:57:20 -0700

    Updating specs from binary to string type

--- lib/poker/poker.ex
+++ lib/poker/poker.ex
@@ -24,42 +24,42 @@ defmodule Poker do
     Poker.GameSession.check(pid, player_id)
   end
 
-  @spec bet(binary(), binary(), binary()) :: %GameState{}
+  @spec bet(String.t(), String.t(), String.t()) :: %GameState{}
   def bet(game_id, player_id, amount) do
     [{pid, nil}] = Registry.lookup(Poker.GameRegistry, game_id)
     Poker.GameSession.bet(pid, player_id, amount)
   end
 
-  @spec new_game_id() :: binary()
+  @spec new_game_id() :: String.t()
   def new_game_id() do
     UUID.generate()
   end
 
-  @spec start_game(binary()) :: %GameState{}
+  @spec start_game(String.t()) :: %GameState{}
   def start_game(game_id) do
     [{pid, nil}] = Registry.lookup(Poker.GameRegistry, game_id)
     Poker.GameSession.start(pid)
   end
 
-  @spec get_game_state(binary()) :: %GameState{}
+  @spec get_game_state(String.t()) :: %GameState{}
   def get_game_state(game_id) do
     [{pid, nil}] = Registry.lookup(Poker.GameRegistry, game_id)
     Poker.GameSession.read(pid)
   end
 
-  @spec join_game(binary(), %Player{}) :: %GameState{}
+  @spec join_game(String.t(), %Player{}) :: %GameState{}
   def join_game(game_id, %Player{} = player) do
     [{pid, nil}] = Registry.lookup(Poker.GameRegistry, game_id)
     Poker.GameSession.join(pid, player)
   end
 
-  @spec create_game(binary(), %Player{}) ::
+  @spec create_game(String.t(), %Player{}) ::
           :ignore | {:error, any()} | {:ok, pid()} | {:ok, pid(), any()}
   def create_game(game_id, %Player{} = player) do
     Poker.GameSupervisor.create_game(game_id, player)
   end
 
-  @spec new_shuffled_deck() :: [%Card{}]
+  @spec new_shuffled_deck() :: [Card.t()]
   def new_shuffled_deck() do
     suits = ["spade", "heart", "club", "diamond"]
 
@@ -90,7 +90,7 @@ defmodule Poker do
     |> Enum.shuffle()
   end
 
-  @spec draw([%Card{}], integer()) :: %{drawn_cards: [%Card{}], new_deck: [%Card{}]}
+  @spec draw([Card.t()], integer()) :: %{drawn_cards: [Card.t()], new_deck: [Card.t()]}
   def draw(deck, draw_count) do
     drawn_cards = Enum.take(deck, draw_count)
     %{drawn_cards: drawn_cards, new_deck: deck -- drawn_cards}
