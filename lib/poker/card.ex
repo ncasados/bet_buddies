803116a5314220bd8963506885ecbbd9404458b8
Nathan Casados <ncasados@live.com>
2025-01-31 19:57:45 -0700

    Updating card from embedded schema to struct

--- lib/poker/card.ex
+++ lib/poker/card.ex
@@ -1,11 +1,15 @@
 defmodule Poker.Card do
-  use Ecto.Schema
+  @type t :: %__MODULE__{
+          suit: String.t(),
+          literal_value: String.t(),
+          high_numerical_value: integer(),
+          low_numerical_value: integer()
+        }
 
-  @primary_key false
-  embedded_schema do
-    field :suit, :string
-    field :literal_value, :string
-    field :high_numerical_value, :integer
-    field :low_numerical_value, :integer
-  end
+  defstruct [
+    :suit,
+    :literal_value,
+    :high_numerical_value,
+    :low_numerical_value
+  ]
 end
